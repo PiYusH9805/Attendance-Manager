263e821cc7ac3a516c89cb36e76f18070984b249
Piyush Sharma <151437982+PiYusH9805@users.noreply.github.com>
2025-12-26 12:29:32 +0530

    Add files via upload

---
+++ userattendance.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package forms;
+
+import com.mongodb.client.AggregateIterable;
+import java.util.List;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+/**
+ *
+ * @author ROG ZEPHYRUS
+ */
+class userattendance {
+
+    static AggregateIterable<Document> aggregate(List<Bson> pipeline) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
